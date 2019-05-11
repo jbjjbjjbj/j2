84327e028692f861a0224422fe46461f8186cf25
Julian T <julian@jtle.dk>
2019-05-11 12:25:43 +0200

    Indent

--- main.c
+++ main.c
@@ -23,84 +23,83 @@ uint16_t *ip = mem;
 uint16_t regA, regB, regC;
 
 void printReg() {
-    printf("A: %d, B: %d, C: %d, IP: %d, i: %c\n", regA, regB, regC, *ip, mem[ *ip ]);
+	printf("A: %d, B: %d, C: %d, IP: %d, i: %c\n", regA, regB, regC, *ip, mem[ *ip ]);
 }
 
 int main( void ) {
-    char c;
-    int i = PROG_BEGIN;
-    *ip = PROG_BEGIN;
+	char c;
+	int i = PROG_BEGIN;
+	*ip = PROG_BEGIN;
 
-    while( ( c = getchar()) != EOF && i < MEM_LEN) {
-	mem[i++] = c;
-    }
-
-    while( *ip ) {
-#if DEBUG == 1
-	printReg();
+	while( ( c = getchar()) != EOF && i < MEM_LEN) {
+		mem[i++] = c;
+	}
+	while( *ip ) { 
+#if DEBUG == 1 
+		printReg();
 #endif
-	*ip = handleChar( mem[ *ip ] );
-    }
+		*ip = handleChar( mem[ *ip ] );
+	}
 
 }
 
 uint16_t handleChar( char c) {
-    uint16_t temp = regA;
+	uint16_t temp = regA;
 
-    switch( c ) {
-    case '+':
-	regA += regB;
-	break;
-    case '-':
-	regA -= regB;
-	break;
-    case 'i':
-	regA ++;
-	break;
-    case 'd':
-	regA --;
-	break;
-    case '*':
-	regA *= regB;
-	break;
-    case 'p':
-	putchar(regA);
-	break;
-    case 'z':
-	regA = regB;
-	regB = temp;
-	break;
-    case 'c':
-	regA = regC;
-	regC = temp;
-	break;
-    case 's':
-	mem_put( regA );
-	break;
-    case 'g':
-	regA = mem_get();
-	break;
-    case 'j':
-	if( regA == 0 ) {
-	    *ip = regC;
-	    regC = 0;
-	    return *ip;
+	switch( c ) {
+		case '+':
+			regA += regB;
+			break;
+		case '-':
+			regA -= regB;
+			break;
+		case 'i':
+			regA ++;
+			break;
+		case 'd':
+			regA --;
+			break;
+		case '*':
+			regA *= regB;
+			break;
+		case 'p':
+			putchar(regA);
+			break;
+		case 'z':
+			regA = regB;
+			regB = temp;
+			break;
+		case 'c':
+			regA = regC;
+			regC = temp;
+			break;
+		case 's':
+			mem_put( regA );
+			break;
+		case 'g':
+			regA = mem_get();
+			break;
+		case 'j':
+			if( regA == 0 ) {
+				*ip = regC;
+				regC = 0;
+				return *ip;
+			}
+			break;
+		case 'l':
+			regA = mem[ ++(*ip) ];
+		default:
+			if( isdigit( c ) )
+				regA = c - '0';
 	}
-	break;
-    case 'l':
-	regA = mem[ ++(*ip) ];
-    default:
-	if( isdigit( c ) )
-	    regA = c - '0';
-    }
 
-    return *ip + 1;
+	return *ip + 1;
 }
 
 void mem_put( uint16_t val ) {
-    mem[regC] = val;
+	mem[regC] = val;
 }
 
 uint16_t mem_get() {
-    return mem[ regC ];
+	return mem[ regC ];
 }
